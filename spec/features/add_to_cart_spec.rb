7bb33ab530d5c13a679e4cf2cdc676915c25dbfa
Luke Bergmann <lukebergmann0@gmail.com>
2021-05-04 20:18:20 +0000

    added test to 'Add to Cart' button

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,35 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor can add a product to cart and cart will increase by 1", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.products.create!(
+        name:  Faker::Hipster.sentence(3),
+        description: Faker::Hipster.paragraph(4),
+        image: open_asset('apparel1.jpg'),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+    visit root_path
+  end
+
+  scenario "Visit the homepage, click on add to card on product, and cart increases by one" do
+    # DEBUG
+    save_screenshot 'Pre_click_on_product.png'
+
+    # find('.product', match: :first).click
+    click_on("Add", match: :first)
+    sleep 1
+    # VERIFY
+    expect(page).to have_css 'nav.navbar'
+
+    save_screenshot 'On_click_on_product.png'
+       
+  end
+end
+
--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,34 +1,34 @@
-require 'rails_helper'
+# require 'rails_helper'
 
-RSpec.feature "Visitor can navigate to product page by clicking on a product", type: :feature, js: true do
+# RSpec.feature "Visitor can navigate to product page by clicking on a product", type: :feature, js: true do
 
-  # SETUP
-  before :each do
-    @category = Category.create! name: 'Apparel'
+#   # SETUP
+#   before :each do
+#     @category = Category.create! name: 'Apparel'
 
-    10.times do |n|
-      @category.products.create!(
-        name:  Faker::Hipster.sentence(3),
-        description: Faker::Hipster.paragraph(4),
-        image: open_asset('apparel1.jpg'),
-        quantity: 10,
-        price: 64.99
-      )
-    end
-    visit root_path
+#     10.times do |n|
+#       @category.products.create!(
+#         name:  Faker::Hipster.sentence(3),
+#         description: Faker::Hipster.paragraph(4),
+#         image: open_asset('apparel1.jpg'),
+#         quantity: 10,
+#         price: 64.99
+#       )
+#     end
+#     visit root_path
+#   end
 
-  end
-  scenario "Visit the homepage, click on a product, and visit the product" do
-        # DEBUG
-        save_screenshot 'Pre_click_on_product.png'
+#   scenario "Visit the homepage, click on a product, and visit the product" do
+#         # DEBUG
+#         save_screenshot 'Pre_click_on_product.png'
 
-        # find('.product', match: :first).click
-        click_on("Details", match: :first)
-        sleep 1
-        # VERIFY
-        expect(page).to have_css 'section.products-show'
+#         # find('.product', match: :first).click
+#         click_on("Details", match: :first)
+#         sleep 1
+#         # VERIFY
+#         expect(page).to have_css 'section.products-show'
 
-        save_screenshot 'On_click_on_product.png'
+#         save_screenshot 'On_click_on_product.png'
            
-      end
-    end
+#       end
+#     end
